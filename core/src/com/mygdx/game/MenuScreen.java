fac07471f8da3b584406712a2c28b7285b897b52
cheyy95 <f1woxihuan@gmail.com>
2019-04-18 07:47:12 +0800

    add code to MyGdxGame and finish MenuScreen

--- core/src/com/mygdx/game/MenuScreen.java
+++ core/src/com/mygdx/game/MenuScreen.java
@@ -4,11 +4,18 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+
 
 public class MenuScreen implements Screen {
     MyGdxGame game; // Note it’s "MyGdxGame" not "Game"
@@ -16,36 +23,79 @@ public class MenuScreen implements Screen {
     private SpriteBatch batch;
     private Skin skin;
     private Stage stage;
+    private Texture texture;
+    private Label label;
+
+
     public MenuScreen(MyGdxGame game){
         this.game = game;
     }
     public void create() {
-        batch = new SpriteBatch();
-        skin = new Skin(Gdx.files.internal("gui/uiskin.json"));
-        stage = new Stage();
-        final TextButton button = new TextButton("Click me", skin, "default");
-        button.setWidth(600f);
-        button.setHeight(400f);
-        button.setPosition(Gdx.graphics.getWidth() /2 - 100f, Gdx.graphics.getHeight()/2 - 10f);
 
-        stage.addActor(button);
+
+        batch = new SpriteBatch();
+        texture = new Texture(Gdx.files.internal("背景图.png"));
+        skin = new Skin(Gdx.files.internal("gui/uiskin.json"));
+        stage = new Stage(new StretchViewport(MyGdxGame.WORLD_WIDTH, MyGdxGame.WORLD_HEIGHT));
+
+        label = new Label("B r e a k o u t", skin, "default");
+        label.setPosition(Gdx.graphics.getWidth() /2 - 195f, Gdx.graphics.getHeight()/2 + 100f);
+        label.setFontScale(2.5f);
+        stage.addActor(label);
+
+        texture = new Texture(Gdx.files.internal("背景图.png"));
+        label = new Label("Help: Enter P to pause the game", skin, "default");
+        label.setPosition(10, 20);
+        label.setFontScale(1.5f);
+        stage.addActor(label);
+
+
+        TextButton playbutton = new TextButton("Play", skin, "default");
+        playbutton.setWidth(150f);
+        playbutton.setHeight(75f);
+        playbutton.setPosition(Gdx.graphics.getWidth() /2 - 150f, Gdx.graphics.getHeight()/2 - 10f);
+        stage.addActor(playbutton);
+        playbutton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
+
+        TextButton exitbutton = new TextButton("Exit", skin, "default");
+        exitbutton.setWidth(150f);
+        exitbutton.setHeight(75f);
+        exitbutton.setPosition(Gdx.graphics.getWidth() /2 - 150f, Gdx.graphics.getHeight()/2 - 100f);
+        stage.addActor(exitbutton);
+        exitbutton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+
+
+            }
+        });
+
+
         Gdx.input.setInputProcessor(stage);
 
 
-        Gdx.app.log("MenuScreen: ","menuScreen create");
+
+
+
+
+
     }
     public void render(float f) {
 
-        Gdx.app.log("MenuScreen: ","menuScreen render");
-        Gdx.app.log("MenuScreen: ","About to call gameScreen");
-        game.setScreen(MyGdxGame.gameScreen);
-        Gdx.app.log("MenuScreen: ","gameScreen started");
 
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        //game.setScreen(MyGdxGame.gameScreen);
+
+        //Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        stage.draw();
+        batch.draw(texture,0,0);
         batch.end();
+        stage.act();
+        stage.draw();
 
     }
     @Override
@@ -58,11 +108,11 @@ public class MenuScreen implements Screen {
     public void resume() { }
     @Override
     public void show() {
-        Gdx.app.log("MenuScreen: ","menuScreen show called");
+
         create();
     }
     @Override
     public void hide() {
-        Gdx.app.log("MenuScreen: ","menuScreen hide called");
+
     }
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,39 +1,28 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends Game implements ApplicationListener {
-	// The class with the menu
-	public static MenuScreen menuScreen;
-	// The class with the game
-	public static GameScreen gameScreen;
+public class MyGdxGame extends Game {
+	public static final float WORLD_WIDTH = 640;
+	public static final float WORLD_HEIGHT = 480;
+
+	SpriteBatch batch;
+	BitmapFont font;
+	Music music;
+
 	@Override
 	public void create() {
-		Gdx.app.log("MyGdxGame: "," create");
-		gameScreen = new GameScreen(this);
-		menuScreen = new MenuScreen(this);
-		Gdx.app.log("MyGdxGame: ","about to change screen to menuScreen");
-// Change screens to the menu
-		setScreen(menuScreen);
-		Gdx.app.log("MyGdxGame: ","changed screen to menuScreen");
-	}
-	@Override
-	public void dispose() {super.dispose();}
-	@Override
-// this method calls the super class render
-// which in turn calls the render of the actual screen being used
-	public void render() {super.render();}
-	@Override
-	public void resize(int width, int height) { super.resize(width, height);}
-	@Override
-	public void pause() {
-		super.pause();
-	}
-	@Override
-	public void resume() {
-		super.resume();
+		music = Gdx.audio.newMusic(Gdx.files.internal("bgm.mp3"));
+		music.setLooping(true);
+		music.setVolume(0.7f);
+		music.play();
+
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		setScreen(new MenuScreen(this));
 	}
 }
-
