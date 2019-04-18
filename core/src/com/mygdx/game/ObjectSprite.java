7cbf41f83f569bee93018554decaef93ecbeeee0
cheyy95 <f1woxihuan@gmail.com>
2019-04-18 10:01:52 +0800

    write defs

--- core/src/com/mygdx/game/ObjectSprite.java
+++ core/src/com/mygdx/game/ObjectSprite.java
@@ -1,8 +1,12 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 public class ObjectSprite {
 
@@ -13,15 +17,30 @@ public class ObjectSprite {
     public ObjectSprite(){
 
     }
-
-
     public void setScreenPosition(float x, float y, float angle) {
         sprite.setX(x);
         sprite.setY(y);
         body.setTransform(new Vector2((x + sprite.getWidth() / 2) / scale, (y + sprite.getHeight() / 2) / scale), angle);
     }
-
     public void setWorldPosition(float x, float y, float angle) {
         body.setTransform(x, y, angle);
+        sprite.setPosition((body.getPosition().x * scale) - sprite.getWidth() / 2, (body.getPosition().y * scale) - sprite.getHeight() / 2);
+        sprite.setRotation((float) Math.toDegrees(body.getAngle()));
+    }
+    public void draw(SpriteBatch batch) {
+        batch.draw(sprite, sprite.getX(), sprite.getY(), sprite.getOriginX(), sprite.getOriginY(), sprite.getWidth(), sprite.getHeight(), sprite.getScaleX(), sprite.getScaleY(), sprite.getRotation());
+    }
+
+    /*------------*/
+    public static class Defs{
+        Texture tex;
+        BodyDef bodyDef;
+        FixtureDef fixtureDef;
+        float x;
+        float y;
+        float scale;
+
+        private Defs(){}
+
     }
 }
