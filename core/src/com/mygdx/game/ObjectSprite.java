36d9482a7243ec3d0159ddad901a8ea2bca3dff2
cheyy95 <f1woxihuan@gmail.com>
2019-04-18 09:38:48 +0800

    Set sprite position in screen and physics world

--- core/src/com/mygdx/game/ObjectSprite.java
+++ core/src/com/mygdx/game/ObjectSprite.java
@@ -1,4 +1,27 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+
 public class ObjectSprite {
+
+    Sprite sprite;
+    Body body;
+    float scale;
+
+    public ObjectSprite(){
+
+    }
+
+
+    public void setScreenPosition(float x, float y, float angle) {
+        sprite.setX(x);
+        sprite.setY(y);
+        body.setTransform(new Vector2((x + sprite.getWidth() / 2) / scale, (y + sprite.getHeight() / 2) / scale), angle);
+    }
+
+    public void setWorldPosition(float x, float y, float angle) {
+        body.setTransform(x, y, angle);
+    }
 }
