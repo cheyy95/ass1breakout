52117eb8882e0f454d985aab5b41f1ff2603f14d
cheyy95 <f1woxihuan@gmail.com>
2019-04-18 11:52:55 +0800

    add def to ball paddle brick ,create edges ,but there are error somewhere, can not run gamescreen

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -11,8 +11,18 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -20,31 +30,43 @@ import com.badlogic.gdx.utils.viewport.StretchViewport;
 public class GameScreen implements Screen {
     MyGdxGame game; // Note it’s "MyGdxGame" not "Game"
     // constructor to keep a reference to the main Game class
-
+    World world;
     Texture padTex;
     Texture ballTex;
     Texture brickTex;
     Sound hitSound;
     Texture texture;
+    Fixture ballhall;
     Stage stage;
-
+    Array<Body> hall;
     ObjectSprite paddle;
     ObjectSprite ball;
     Array<ObjectSprite> bricks;
+    Array<ObjectSprite> remove;
 
 
     int screenWidth;
     int screenHeight;
     static final float WORLD_SCALE = 100f;
+
+    // collision
+    static final short WORLD_BOUND = 1;
+    static final short PADDLE = 1 << 1;
+    static final short BRICK = 2;
+    static final short BALL = 1;
+    static final short bottomfail = 3;
+
     private int score = 0;
     private int add;
     private int lives = 0;
+
+    private boolean placeball = false;
 
     GameScreen(MyGdxGame game) {
         this(game, 0, 1, 0);
     }
 
-    GameScreen(MyGdxGame game, int score, int add, int lives){
+    GameScreen(MyGdxGame game, int score, int add, int lives) {
         this.game = game;
         this.game = game;
         this.score = score;
@@ -60,13 +82,55 @@ public class GameScreen implements Screen {
         hitSound = Gdx.audio.newSound(Gdx.files.internal("hit.mp3"));
         texture = new Texture(Gdx.files.internal("背景图.png"));
 
+        // create physics world
+        world = new World(new Vector2(0, 0), true);
+
         bricks = new Array<ObjectSprite>();
+
+        world.setContactListener(new ContactListener() {
+
+            @Override
+            public void beginContact(Contact contact) {
+                Fixture fixa = contact.getFixtureA();
+                Fixture fixb = contact.getFixtureB();
+
+                for (ObjectSprite brick : bricks) {
+                    if ((fixa == brick.fixture && fixb == ball.fixture)
+                            || (fixa == ball.fixture && fixb == brick.fixture)) {
+                        remove.add(brick);
+                    }
+                }
+
+                if ((fixa == ballhall && fixb == ball.fixture)
+                        || (fixa == ball.fixture && fixb == ballhall)) {
+                    placeball = true;
+                }
+            }
+
+            @Override
+            public void endContact(Contact contact) {
+            }
+
+            @Override
+            public void preSolve(Contact contact, Manifold oldManifold) {
+            }
+
+            @Override
+            public void postSolve(Contact contact, ContactImpulse impulse) {
+            }
+        });
+
+        createEdges();
+        createPaddle();
+        createBall();
+        createBricks();
     }
+
     public void create() {
 
 
-
     }
+
     public void render(float f) {
 
         if (lives-- == 0) {
@@ -76,42 +140,74 @@ public class GameScreen implements Screen {
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
-            game.setScreen(new PausedScreen(game,this));
+            game.setScreen(new PausedScreen(game, this));
         }
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
     }
+
     @Override
-    public void dispose() { }
+    public void dispose() {
+    }
+
     @Override
-    public void resize(int width, int height) { }
+    public void resize(int width, int height) {
+    }
+
     @Override
-    public void pause() { }
+    public void pause() {
+    }
+
     @Override
-    public void resume() { }
+    public void resume() {
+    }
+
     @Override
     public void show() {
-        Gdx.app.log("GameScreen: "," show called");
+        Gdx.app.log("GameScreen: ", " show called");
         create();
     }
+
     @Override
     public void hide() {
     }
 
     private void createBall() {
-        float x = (screenWidth / 2)-5;
+        float x = (screenWidth / 2) - 5;
         float y = 45;
 
         ObjectSprite.Defs defs = ObjectSprite.Defs.fromScreenCoordinates(
-                ballTex, x, y, WORLD_SCALE);}
+                ballTex, x, y, WORLD_SCALE);
+        defs.bodyDef.type = BodyDef.BodyType.DynamicBody;
+
+        defs.fixtureDef.friction = 0;
+        defs.fixtureDef.density = 0.1f;
+        defs.fixtureDef.restitution = 1f;
+        defs.fixtureDef.filter.categoryBits = BALL;
+        defs.fixtureDef.filter.maskBits = (PADDLE | BRICK | WORLD_BOUND | bottomfail);
+
+        ball = new ObjectSprite(defs, world);
+    }
 
     private void createPaddle() {
         float x = (screenWidth / 2) - (padTex.getWidth() / 2);
         float y = 32;
         ObjectSprite.Defs defs = ObjectSprite.Defs.fromScreenCoordinates(
-                padTex, x, y, WORLD_SCALE);}
+                padTex, x, y, WORLD_SCALE);
+
+        defs.bodyDef.type = BodyDef.BodyType.DynamicBody;
+        defs.bodyDef.fixedRotation = true;
+
+        defs.fixtureDef.density = 0.1f;
+        defs.fixtureDef.filter.categoryBits = PADDLE;
+        defs.fixtureDef.filter.maskBits = (BALL | WORLD_BOUND);
+
+        paddle = new ObjectSprite(defs, world);
+        paddle.body.setLinearDamping(2.0f);
+    }
+
 
     private void createBricks() {
 
@@ -128,10 +224,53 @@ public class GameScreen implements Screen {
 
                 ObjectSprite.Defs defs = ObjectSprite.Defs.fromScreenCoordinates(
                         brickTex, col, row, WORLD_SCALE);
+
+                defs.bodyDef.type = BodyDef.BodyType.DynamicBody;
+
+                defs.fixtureDef.density = 0.1f;
+                defs.fixtureDef.filter.categoryBits = BRICK;
+                defs.fixtureDef.filter.maskBits = (BALL | WORLD_BOUND);
+
+                ObjectSprite brick = new ObjectSprite(defs, world);
+
+                bricks.add(brick);
             }
 
         }
 
-        
+
+    }
+
+    private void createEdges() {
+        float w = Gdx.graphics.getWidth() / WORLD_SCALE;
+        float h = Gdx.graphics.getHeight() / WORLD_SCALE;
+        for (Vector2[] points : new Vector2[][]{
+                {new Vector2(0, 0), new Vector2(0, h)}, // left
+                {new Vector2(w, 0), new Vector2(w, h)}, // right
+                {new Vector2(0, 0), new Vector2(w, 0)}, // top
+                {new Vector2(0, h), new Vector2(w, h)}} // bottom
+        ) {
+            Vector2 p1, p2;
+            p1 = points[0];
+            p2 = points[1];
+
+
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(0f, 0f);
+
+            EdgeShape shape = new EdgeShape();
+            shape.set(p1, p2);
+
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = shape;
+            fixtureDef.filter.categoryBits = WORLD_BOUND;
+
+            Body body = world.createBody(bodyDef);
+            body.createFixture(fixtureDef);
+
+            hall.add(body);
+            shape.dispose();
+        }
     }
 }
