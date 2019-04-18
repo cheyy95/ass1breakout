0fa0743acc27e2fc5aad5a389ec45594834ec307
cheyy95 <f1woxihuan@gmail.com>
2019-04-18 10:10:59 +0800

    create Body and Fixture Def

--- core/src/com/mygdx/game/ObjectSprite.java
+++ core/src/com/mygdx/game/ObjectSprite.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 
 public class ObjectSprite {
 
@@ -40,7 +41,41 @@ public class ObjectSprite {
         float y;
         float scale;
 
-        private Defs(){}
+        private Defs(){
+            Defs defs = new Defs();
+            defs.tex = tex;
+            defs.x = x;
+            defs.y = y;
+            defs.scale = scale;
+
+            defs.bodyDef = new BodyDef();
+            defs.bodyDef.position.set(
+                    (x + tex.getWidth() / 2) / scale,
+                    (y + tex.getHeight() / 2) / scale);
+
+            defs.fixtureDef = new FixtureDef();
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(
+                    tex.getWidth() / 2 / scale,
+                    tex.getHeight() / 2 / scale);
+            defs.fixtureDef.shape = shape;
+
+            Defs defs = new Defs();
+            defs.tex = tex;
+            defs.x = x * scale;
+            defs.y = y * scale;
+            defs.scale = scale;
+
+            defs.bodyDef = new BodyDef();
+            defs.bodyDef.position.set(x, y);
+
+            defs.fixtureDef = new FixtureDef();
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(
+                    tex.getWidth() / 2 / scale,
+                    tex.getHeight() / 2 / scale);
+            defs.fixtureDef.shape = shape;
+        }
 
     }
 }
