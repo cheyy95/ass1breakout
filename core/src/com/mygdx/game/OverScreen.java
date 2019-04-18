1d509a88e3ffa0f23a5da2b1a96473d507f437ae
cheyy95 <f1woxihuan@gmail.com>
2019-04-18 08:53:51 +0800

    continue to comliete overscreen,add button retry and quit.now almost finish all screen stuff,cant test overscreen rightnow ,because unable to trigger now, but it should be working

--- core/src/com/mygdx/game/OverScreen.java
+++ core/src/com/mygdx/game/OverScreen.java
@@ -5,9 +5,12 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class OverScreen implements Screen {
@@ -37,6 +40,34 @@ public class OverScreen implements Screen {
         float labelY = MyGdxGame.WORLD_HEIGHT / 3 * 2 + label.getHeight() / 2;
         label.setPosition(labelX, labelY);
         stage.addActor(label);
+
+        TextButton retryButton = new TextButton("RETRY", skin);
+        retryButton.setWidth(150f);
+        retryButton.setHeight(75f);
+        retryButton.setPosition(Gdx.graphics.getWidth() /2 - 150f, Gdx.graphics.getHeight()/2 - 10f);
+        retryButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                game.setScreen(new GameScreen(game));
+            }
+        });
+        stage.addActor(retryButton);
+
+        TextButton quitButton = new TextButton("QUIT", skin);
+        quitButton.setWidth(150f);
+        quitButton.setHeight(75f);
+        quitButton.setPosition(Gdx.graphics.getWidth() /2 - 150f, Gdx.graphics.getHeight()/2 - 100f);
+        quitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                Gdx.app.exit();
+            }
+        });
+        stage.addActor(quitButton);
+
+        Gdx.input.setInputProcessor(stage);
 
     }
 
